62cdd3bf554162a4c58438fb20adca661025ab51
vikram kumar <vikram.1rn14ec170@gmail.com>
2022-01-08 15:37:23 +0530

    add Sign in and sign up fix

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Header from "./header/header";
 import LandingPage from "./landingPage/landingPage";
+import SignIn from "./auth/signIn/signIn";
+import SignUp from "./auth/signUp/signUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export class Main extends Component {
@@ -10,7 +12,8 @@ export class Main extends Component {
         <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          
+          <Route path="signIn" element={<SignIn />} />
+          <Route path="SignUp" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     );
